6d9a894ebea8479fa642491dacb0186e642bdb59
Jayde Livingstone <livingst@gpid16axni-01.esrf.fr>
2023-02-23 11:12:14 +0100

    Fixed bug in bit depth conversion

--- vol2raw.py
+++ vol2raw.py
@@ -34,7 +34,6 @@ class ht_vol:
 		self.voxelSize = float(self.info["voxelSize"])
 		self.vmin = float(self.info["ValMin"])
 		self.vmax = float(self.info["ValMax"])
-		self.shape = str(self.rows) + "x" + str(self.cols) + "x" + str(self.slices)
 		
 		self.fromType = "float32"
 		self.toType = dt[args.bitdepth]
@@ -44,8 +43,6 @@ class ht_vol:
 		strbits = args.bitdepth
 		
 		self.saveFile = args.volume.replace(".vol", strbits).split("/")[-1]
-		
-		print("\n"+str(self.volName))
 
 
 	def get_vol_info(self, infoFile):
@@ -79,7 +76,7 @@ class ht_vol:
 			
 			
 	def read_vol(self):
-		print("Processing volume.")
+		print("Processing volume " + self.volName)
 		self.orig = np.memmap(self.volName, dtype=self.fromType, mode='r', offset=0, shape=(self.slices*self.rows*self.cols))
 		
 		if self.toType != "float32":
@@ -90,55 +87,69 @@ class ht_vol:
 		self.flag = "vol"
 
 	
+	def pi_image(self):
+		self.x = round(self.cols * self.zoom)
+		self.y = round(self.rows * self.zoom)
+		self.z = round(self.slices * self.zoom)
+		
+		self.shape = str(self.y) + "x" + str(self.x) + "x" + str(self.z)
+		
+		pi_cmd = {"float32" : self.pi.newimage(ImageDataType.FLOAT32, self.y, self.x, self.z), 
+					"uint32" : self.pi.newimage(ImageDataType.UINT32, self.y, self.x, self.z), 
+					"uint16" : self.pi.newimage(ImageDataType.UINT16, self.y, self.x, self.z), 
+					"uint8" : self.pi.newimage(ImageDataType.UINT8, self.y, self.x, self.z)}
+					
+		self.pi_img = pi_cmd[self.toType]
+	
 	def convert(self):
 		intbits = {"uint32" : 32, "uint16" : 16, "uint8" : 8}
 		print("Converting to " + str(intbits[self.toType]) + " bits.")
 		self.orig = self.orig.copy()
-		self.orig[self.orig == 0] = self.vmin - 1 # Zero-valued pixels (eg. pixels outside the reconstructed cylinder) should retain their zero-value after the conversion
+		self.vmin -= 1.
+		self.orig[self.orig == 0] = self.vmin # Zero-valued pixels (eg. pixels outside the reconstructed cylinder) should retain their zero-value after the conversion
+		self.orig[self.orig >= self.vmax] = self.vmax
+		self.orig[self.orig <= self.vmin] = self.vmin
 		self.orig = ((self.orig - self.vmin) / (self.vmax - self.vmin)) * (2 ** intbits[self.toType] - 1)
 		self.orig = self.orig.astype(self.toType)
 		
 	
 	def reshape_img(self, slices, rows, cols):
-		print("Reshaping image data.")
+		print("Reshaping volume from numPy (slices, rows, cols) format to pi2 format (rows, cols, slices).")
 		self.new = self.new.astype(self.toType)
 		self.new = self.new.reshape(slices, rows, cols)
 		self.new = np.transpose(self.new, (1, 2, 0))
 		
 
-	def magnify(self):
-		print("Magnifying image.")
-		self.new = ndimage.zoom(self.new, self.zoom, order=2)
-		self.rows, self.cols, self.slices = self.new.shape
-		self.saveFile += str(int(self.voxelSize / self.zoom))
+	def rescale_img(self):
+		toSize = self.voxelSize / self.zoom * 1000. # convert to nm
+		print("Rescaling to " + str(toSize) + " nm pixel/voxel size.")
+		self.pi.scale(self.new, self.pi_img)
+		self.new = self.pi_img.get_data()
+		self.saveFile += "_rescaled_"+str(int(toSize)) + "nm"
 		
 
 	def write_to_raw(self):
 		print("Writing to ./" + str(self.saveFile) + "_" + self.shape + ".raw")
-		pi_cmd = {"float32" : self.pi.newimage(ImageDataType.FLOAT32, self.cols, self.rows, self.slices), 
-					"uint32" : self.pi.newimage(ImageDataType.UINT32, self.cols, self.rows, self.slices), 
-					"uint16" : self.pi.newimage(ImageDataType.UINT16, self.cols, self.rows, self.slices), 
-					"uint8" : self.pi.newimage(ImageDataType.UINT8, self.cols, self.rows, self.slices)}
-					
-		self.raw_img = pi_cmd[self.toType]
-		self.raw_img.set_data(self.new)
+		self.pi_img.set_data(self.new)
 		
 		if self.flag == "vol":
-			self.pi.writeraw(self.raw_img, self.saveFile)
-		elif self.flag == "slice"
-			self.pi.writerawblock(self.raw_img, self.saveFile, [0, 0, self.sliceID], [self.cols, self.rows, self.slices]) 
+			self.pi.writeraw(self.pi_img, self.saveFile)
+		elif self.flag == "slice":
+			self.pi.writerawblock(self.pi_img, self.saveFile, [0, 0, self.sliceID], [self.cols, self.rows, self.slices]) 
 		
 		
 	def vol_to_raw(self):
 		self.read_vol()
+		self.pi_image()
 		if self.zoom > 1:
-			self.magnify()
+			self.rescale_img()
 		self.write_to_raw()
 		
 		
 	def slice_to_raw(self, sliceID, totalSlices):
 		self.slices = totalSlices
 		self.read_slice(sliceID)
+		self.pi_image()
 		if self.zoom > 1:
 			self.magnify()
 		self.write_to_raw()
